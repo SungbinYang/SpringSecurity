e7252a26a557831eb5be3558f80684d2affd46a0
SungbinYang <ysb05222@naver.com>
2022-02-17 21:39:28 +0900

    스프링 시큐리티 설정하기

---
+++ demo-spring-security-form/src/main/java/me/sungbin/demospringsecurityform/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package me.sungbin.demospringsecurityform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * packageName : me.sungbin.demospringsecurityform.config
+ * fileName : SecurityConfig
+ * author : rovert
+ * date : 2022/02/17
+ * description :
+ * ===========================================================
+ * DATE 			AUTHOR			 NOTE
+ * -----------------------------------------------------------
+ * 2022/02/17       rovert         최초 생성
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
